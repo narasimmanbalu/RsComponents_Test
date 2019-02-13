85c27b9fd614e18aad7194fe45a98e70e5d6c571
narasimmanbalu <balu.testanalyst@gmail.com>
2019-02-13 18:39:09 +0000

    code cleanup

--- src/test/java/stepDefinitions/Steps.java
+++ src/test/java/stepDefinitions/Steps.java
@@ -15,7 +15,7 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
-public class E2E_Test_Steps {
+public class Steps {
 
 	WebDriver driver;
 	WebDriverManager webDriverManager;
